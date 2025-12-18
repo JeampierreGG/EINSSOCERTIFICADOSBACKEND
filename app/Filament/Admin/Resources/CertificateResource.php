373f410b7def6cbd16c14ca8f7a28f41e2b90c89
Jeampierre <ajeampiergg10@gmail.com>
2025-12-18 00:44:37 -0500

    Actualización del calendario, campo fecha de emisión en el panel de adminsitración al asignar certificados

--- app/Filament/Admin/Resources/CertificateResource.php
+++ app/Filament/Admin/Resources/CertificateResource.php
@@ -183,7 +183,7 @@ class CertificateResource extends Resource
                             ]),
                         Forms\Components\Grid::make(3)
                             ->schema([
-                                Forms\Components\DatePicker::make('issue_date')->label('Fecha de emisión')->required()->native(false)->displayFormat('d/m/Y')->firstDayOfWeek(1),
+                                Forms\Components\DatePicker::make('issue_date')->label('Fecha de emisión')->required()->native(true)->displayFormat('d/m/Y')->firstDayOfWeek(1),
                                 Forms\Components\TextInput::make('code')
                                     ->label('Código de certificado')
                                     ->required()
@@ -267,7 +267,7 @@ class CertificateResource extends Resource
                                     ]),
                                 Forms\Components\Grid::make(3)
                                     ->schema([
-                                        Forms\Components\DatePicker::make('issue_date')->label('Fecha de emisión')->required()->native(false)->displayFormat('d/m/Y')->firstDayOfWeek(1),
+                                        Forms\Components\DatePicker::make('issue_date')->label('Fecha de emisión')->required()->native(true)->displayFormat('d/m/Y')->firstDayOfWeek(1),
                                         Forms\Components\TextInput::make('code')
                                             ->label('Código de certificado')
                                             ->required()
