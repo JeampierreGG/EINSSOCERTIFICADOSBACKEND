e3b486a9347382b864954529e63b06168602db1b
Jeampierre <ajeampiergg10@gmail.com>
2026-03-01 01:51:14 -0500

    build: generate correct composer.lock

--- clean_attempts.sql
+++
@@ -1,3 +0,0 @@
--- Limpiar attempts completados de la evaluación 1
-DELETE FROM evaluation_answers WHERE evaluation_attemps_id IN (SELECT id FROM evaluation_attempts WHERE evaluation_id = 1);
-DELETE FROM evaluation_attempts WHERE evaluation_id = 1;
